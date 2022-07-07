b90453721bbb9644ab0d8870b155df6a5b14d7eb
Mohammad Al Saleh <mo.alsaleh95@gmail.com>
2022-07-08 02:26:05 +0300

    done

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,30 +1,3 @@
-// import React, { useState } from "react";
-// import { spicyFoods, getNewSpicyFood } from "../data";
-
-// function SpicyFoodList() {
-//   const [foods, setFoods] = useState(spicyFoods);
-
-//   function handleAddFood() {
-//     const newFood = getNewSpicyFood();
-//     console.log(newFood);
-//   }
-
-//   const foodList = foods.map((food) => (
-//     <li key={food.id}>
-//       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
-//     </li>
-//   ));
-
-//   return (
-//     <div>
-//       <button onClick={handleAddFood}>Add New Food</button>
-//       <ul>{foodList}</ul>
-//     </div>
-//   );
-// }
-
-// export default SpicyFoodList;
-
 import React, { useState } from "react";
 import { spicyFoods, getNewSpicyFood } from "../data";
 
@@ -41,6 +14,7 @@ function SpicyFoodList() {
   function handleLiClick(id) {
     const newFoodArray = foods.map((food) => {
       if (food.id === id) {
+        console.log(food)
         return {
           ...food,
           heatLevel: food.heatLevel + 1,
